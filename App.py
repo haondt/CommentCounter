f15ed4d90c3c897676d58eb55516d57387b65ca2
Noah <burghardtnoah@gmail.com>
2022-02-16 19:56:45 -0700

    added logging to file

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ import threading
 from CommentFormatter import CommentFormatter
 from JobLocater import JobLocater
 from JobRunner import JobRunner
-from Logger import Logger
+from Logger import ConsoleSink, FileSink, Logger
 from Models.State import State
 import JsonExtended as json
 import signal
@@ -12,9 +12,11 @@ import os
 from Storage import FileStorage
 
 activeJobFile = "activeJobs.json"
+logFile = "log.txt"
 username = "CountTheComments"
 updateInterval = timedelta(hours=1)
 numUpdates = 24
+
 
 reddit = praw.Reddit('bot1')
 reddit.validate_on_submit=True
@@ -23,6 +25,9 @@ mutex = threading.Lock()
 run_event = threading.Event()
 
 storage = FileStorage(activeJobFile)
+consoleSink = ConsoleSink()
+fileSink = FileSink(logFile)
+loggerFactory = lambda x: Logger([consoleSink, fileSink], x)
 
 jobRunner = JobRunner(
     run_event,
@@ -32,7 +37,7 @@ jobRunner = JobRunner(
     reddit,
     updateInterval,
     CommentFormatter(),
-    Logger("Runner")
+    loggerFactory("Runner")
 )
 
 jobLocater = JobLocater(
@@ -43,7 +48,7 @@ jobLocater = JobLocater(
     reddit,
     jobRunner.RunNewJobs,
     numUpdates,
-    Logger("Locater")
+    loggerFactory("Locater")
 )
 
 jobRunnerThread = threading.Thread(target=jobRunner.run)
--- Logger.py
+++ Logger.py
@@ -1,20 +1,36 @@
 from datetime import datetime
 from os import pread
+class ConsoleSink:
+    def write(self, message):
+        print(message)
+
+class FileSink:
+    def __init__(self, filename):
+        self._filename = filename
+
+    def write(self, message):
+        with open(self._filename, "a", encoding="UTF-8") as f:
+            f.write(message + "\n")
+
 class Logger:
-    def __init__(self, context=None):
+    def __init__(self, sinks=[], context=None):
+        self._sinks = sinks
         self._context = context
 
+    def _log(self, message):
+        for sink in self._sinks:
+            sink.write(message)
+
     def log(self, message):
-        print(self._format(message))
+        self._log(self._format(message))
+
     def log_error(self, message, error):
-        print(self._format(f"{message}\n{error}"))
+        self._log(self._format(f"{message}\n{error}"))
 
     def _format(self, message):
         parts = []
         parts.append("[" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "]")
         if self._context is not None:
             parts.append(f"[{self._context}]")
-
         parts.append(message)
-
         return " ".join(parts)
